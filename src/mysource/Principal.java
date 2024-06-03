0fc2f9c853896dd5759c90261697187a0956885e
TrianaSolerMartín <152178100+TrianaSolerMartin@users.noreply.github.com>
2024-06-03 15:01:40 +0200

    src-first-java-structure

---
+++ src/mysource/Principal.java
@@ -0,0 +1,17 @@
+package mysource;
+
+public class Principal {
+/*Main es el método o (función)main es el método principal y es el punto e entrada de toda la aplicación como el main.jsx */ 
+    public static void main(String[] args) {
+      System.out.println("HELLO WORLD, HOW DO U FELL TODAY?");
+      /*console.log*/
+    }
+   /*compilación y generar el punto clas
+    RUN FILE*/    
+}
+
+/*TOda clase comienza en mayusculas, en el ejemplo: Principal, System, String 
+-todo lo que venga despues de la clase con un punto es un método
+
+ejemplo: main println
+*/
---
+++ src/mysource/Principal2.java
@@ -0,0 +1,16 @@
+package mysource;
+
+public class Principal2 {
+
+    public static void main(String[] args) {
+        String name = "Triana";
+        int number = 5;
+        double note = 6.5;
+        float admition = 6.5f;
+
+        System.out.println(name.toUpperCase());
+        System.out.print(number);
+        System.out.println(note);
+    }
+    
+}
