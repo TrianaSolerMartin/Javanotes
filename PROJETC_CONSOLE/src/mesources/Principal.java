c6beafeb2c481783134aca3af66a318bfc913ac7
TrianaSolerMartin <triana287@gmail.com>
2024-06-04 11:07:52 +0200

    folder java added

---
+++ PROJETC_CONSOLE/src/mesources/Principal.java
@@ -0,0 +1,16 @@
+package mesources;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        char letter = 'A';
+                System.out.println("Código ASCII: " +(int)letter);
+                System.out.println("Codigo LETRA: " + letter);
+                
+                //COMO IMPRIMIR LASLTRAS DEL ALFABETO
+                for(int i=65; i<=90; i++) {
+                    System.out.println((char)i);
+                }
+                    
+    }
+}
