9e5f6ace12eec78334e250e4115f6014e62ade00
TrianaSolerMartin <triana287@gmail.com>
2024-06-04 11:32:32 +0200

    binary to decimal

--- PROJETC_CONSOLE/src/mesources/Principal3.java
+++ PROJETC_CONSOLE/src/mesources/Principal3.java
@@ -1,20 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package mesources;
 
-/**
- *
- * @author Administrator
- */
 public class Principal3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+          int binario = 100101;
+          //convertimos a cadena de texto
+          
+          String sbinario = String.valueOf(binario);//"100101"
+          int decimal = Integer.parseInt(sbinario.charAt(0)+"") * (int)Math.pow(2,5);
+          int decima2 = Integer.parseInt(sbinario.charAt(3)+ "") * (int)Math.pow(2,2);
+          int decima3 = Integer.parseInt(sbinario.charAt(5)+ "") * (int)Math.pow(2,0);
+            System.out.println("Resultado: " + (decimal+decima2+decima3));
     }
     
 }
+
+/* Math.pow            POTENCIA                         double
+ObjetoCadena.charAt(0) obtener un caracter e una cadena char
+String.valueOf         Convierte número a cadena        String
+*/
