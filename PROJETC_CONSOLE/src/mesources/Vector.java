63ea4a247073c4a139f5fc54f00daaa08bf1060a
TrianaSolerMartin <triana287@gmail.com>
2024-06-07 11:42:37 +0200

    vector added

---
+++ PROJETC_CONSOLE/src/mesources/Vector.java
@@ -0,0 +1,16 @@
+package mesources;
+
+// @author Administrator
+public class Vector {
+
+    public static void main(String[] args) {
+        double n1 = 5.6;
+        double n2 = 5.6; 
+        double n3 = 8.1;
+        double n4 = 5.6;
+        double n5 = 8.1;
+                        // 0     1   2    3     4 
+        double [] nota = {5.6, 5.6, 8.1, 5.6, 8.1};
+        System.out.println("Nota 1: " + nota[0]);
+    }
+}
