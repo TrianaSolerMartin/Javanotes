92e467efd8d66da2b6e86e801a7f670f30fad2ae
TrianaSolerMartin <triana287@gmail.com>
2024-06-07 12:04:59 +0200

    try catch java

--- PROJETC_CONSOLE/src/mesources/Vector.java
+++ PROJETC_CONSOLE/src/mesources/Vector.java
@@ -4,13 +4,25 @@ package mesources;
 public class Vector {
 
     public static void main(String[] args) {
-        double n1 = 5.6;
+        /*double n1 = 5.6;
         double n2 = 5.6; 
         double n3 = 8.1;
         double n4 = 5.6;
-        double n5 = 8.1;
+        double n5 = 8.1;*/
                         // 0     1   2    3     4 
         double [] nota = {5.6, 5.6, 8.1, 5.6, 8.1};
         System.out.println("Nota 1: " + nota[0]);
+        //manejo de errores
+        try {
+            System.out.println("Nota 6: " +nota[5]);
+            //desbordamiento del array
+        } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println("No se ha encontrado todos los datos requeeridos en la lista");
+    }
+        double suma = 0.0;
+        for (int i=0; i<nota.length; i++){
+            suma = suma +nota [i];
+        }
+        System.out.println("PRomedio: " + suma/nota.length);
     }
 }
