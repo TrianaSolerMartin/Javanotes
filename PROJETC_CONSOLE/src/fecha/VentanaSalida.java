990b0be7a3029ab0da45fa1f6b5a78dae95d890f
TrianaSolerMartin <triana287@gmail.com>
2024-06-20 12:52:47 +0200

    rool added

--- PROJETC_CONSOLE/src/fecha/VentanaSalida.java
+++ PROJETC_CONSOLE/src/fecha/VentanaSalida.java
@@ -1,9 +1,11 @@
 package fecha;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 public class VentanaSalida extends JFrame {
     JTextArea txaContenido = new JTextArea();
+    JScrollPane scrollpaanel = new JScrollPane();
     
     
 }
