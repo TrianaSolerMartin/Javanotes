5aebddf9d8df603242a8809e93cf5b0de16708db
TrianaSolerMartin <triana287@gmail.com>
2024-06-25 14:11:38 +0200

    validation example addedergferg

---
+++ PROJETC_CONSOLE/src/swing/VentanaOutput.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package swing;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Administrator
+ */
+public class VentanaOutput extends JFrame {
+
+    public VentanaOutput() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
