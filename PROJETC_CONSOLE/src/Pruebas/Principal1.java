81bd08c267a0d5373b84b472d9c95173f66b40b0
TrianaSolerMartin <triana287@gmail.com>
2024-07-03 10:25:09 +0200

    arraylisst added

--- PROJETC_CONSOLE/src/Pruebas/Principal1.java
+++ PROJETC_CONSOLE/src/Pruebas/Principal1.java
@@ -2,15 +2,20 @@
 package Pruebas;
 
 // @author Administrator
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class Principal1 {
 
     public static void main(String[] args) {
-        List<String> nombre_al = new ArrayList<>();
+        List<String> nombres_al = new ArrayList<>();
+
+        nombres_al.add("Juan");
+        nombres_al.add("Pedro");
+        nombres_al.add("Maria");
+        nombres_al.add("Jose");
     
-    for (String elemento : nombre_al){
+    for (String elemento : nombres_al){
         System.out.println(elemento);
     }}
 }
