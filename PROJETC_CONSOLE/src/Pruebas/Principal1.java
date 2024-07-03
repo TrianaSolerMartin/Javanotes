5ed6153f29c78b91ba468c7dcb8f947bf9d43f04
TrianaSolerMartin <triana287@gmail.com>
2024-07-03 10:11:57 +0200

    validation example addedergferg

---
+++ PROJETC_CONSOLE/src/Pruebas/Principal1.java
@@ -0,0 +1,16 @@
+
+package Pruebas;
+
+// @author Administrator
+
+import java.util.List;
+
+public class Principal1 {
+
+    public static void main(String[] args) {
+        List<String> nombre_al = null;
+    
+    for (String elemento : nombre_al){
+        System.out.println(elemento);
+    }}
+}
